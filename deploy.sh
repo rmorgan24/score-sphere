2ae99b11bd47cc4076dc58bb28c12c23a8c15b26
Brian Morgan <brian.s.morgan@gmail.com>
2024-12-30 10:20:27 -0500

    add lock to deploy process

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
+LOCKFILE="/tmp/deploy.lock"
+
+# Check if the lock file exists
+if [ -e "$LOCKFILE" ]; then
+    echo "Script is already running. Exiting."
+    exit 1
+fi
+
+# Create a lock file
+trap "rm -f $LOCKFILE; exit" INT TERM EXIT
+touch "$LOCKFILE"
+
 git pull
 export STATIC_VERSION=`git rev-parse --short HEAD`
 docker compose --profile $1 build
 docker compose --profile $1 up -d
+
+# Remove the lock file
+rm -f "$LOCKFILE"
+trap - INT TERM EXIT
